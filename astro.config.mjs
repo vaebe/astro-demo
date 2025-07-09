4226736c2d666cdadd942cf0206b0a212d92c71f
vae <18137693952@163.com>
2025-07-09 17:08:27 +0800

    feat: 增加 tailwindcss 集成

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,14 @@ import { defineConfig } from 'astro/config';
 
 import preact from "@astrojs/preact";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-d.netlify.app",
-  integrations: [preact()]
+  integrations: [preact()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
